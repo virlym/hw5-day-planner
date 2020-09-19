d233b6fed4a75cdcdec114652661f1802fd38f5f
Devin Gillogly <virlym@gmail.com>
2020-09-18 17:01:40 -0700

    updated comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # hw5-day-planner
+
+A nine-to-five daily planner
+
+Key code includes:
+1) Current day is displayed at the top of the planner
+2) Time blocks are created for the business hours of 9-5
+3) Each time block is color coded to show if it is past, present or future
+4) Each time block has an editable note section
+5) Clicking the save button saves the notes for that time block
+6) Saved notes persist upon refreshing the screen
+
+github repo : https://github.com/virlym/hw5-day-planner
+
+deployed page : https://virlym.github.io/hw5-day-planner/
+
+Preview:
+
+![plannerExample](https://github.com/virlym/hw5-day-planner/blob/master/Assets/plannerExample.PNG)
--- script.js
+++ script.js
@@ -1,93 +1,146 @@
-console.log(moment().format());
-console.log(moment().format("h A"));
-
-console.log(moment().format("HH"));
-var holder = $(".container");
-
+// Main populating function
 function init(){
-    for (i = 9; i < 18; i++){
-        var hour = i;
-        var split = "AM";
-        var status = "present";
-        var rowId = ["9am", "10am", "11am", "noon", "1pm", "2pm", "3pm", "4pm", "5pm"];
-        var reminderText = "";
-        if(i > 12){
-            hour -= 12;
-        }
-        if(i > 11){
-            split = "PM";
-        }
+    // set the current time to the top of the planner
+    var currentTime = $("<h2>");
+    currentTime.text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
+    $("#currentDay").append(currentTime);
 
-        if(i < moment().format("HH")){
-            status = "past";
-        }
-        if(i > moment().format("HH")){
-            status = "future";
-        }
-        if(getSavedInfo(rowId[i-9])){
-            reminderText = getSavedInfo(rowId[i-9]);
-        }
-        var newRow = $("<section>");
-        newRow.attr("class", `row time-block`);
-        newRow.attr("id", `${rowId[i-9]}`);
-        var col1 = $("<article>");
-        col1.attr("class", "col-mid-2 hour");
-        col1.text(`${hour} ${split}`);
-        var text = $("<textarea>");
-        text.attr("class", `col-md-8 ${status}`);
-        text.text(reminderText);
-        var save = $("<button>");
-        save.attr("class", "col-md-2 saveBtn");
-        save.on("click", function(){ 
-            saveInfo(($(this).parent().attr("id")), ($.trim($(this).prev().val())));
-            resetPage();
-         });
-        save.text("Save");
-        newRow.append(col1);
-        newRow.append(text);
-        newRow.append(save);
-        holder.append(newRow);
+    // create a time block for the hours of 9am - 5pm
+    for (i = 9; i < 18; i++){
+        addTimeBlock(i);
     }
 
-        var endRow = $("<section>");
-        endRow.attr("class", `row`);
-        var emptyOneCol = $("<article>");
-        emptyOneCol.attr("class", `col-md-2`);
-        var finalCol = $("<div>");
-        finalCol.attr("class", `col-md-6`);
-        var clear = $("<button>");
-        clear.text("clear all");
-        clear.attr("class", "btn-block");
-        clear.on("click", function() { clearAll() });
-        finalCol.append(clear);
-        endRow.append(emptyOneCol, finalCol);
-        holder.append(endRow);
-
+    // add a clear button
+    addClear();
 }
 
+function addTimeBlock(index){
+    // create some base id references
+    var rowId = ["9am", "10am", "11am", "noon", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+    // create a new row with a referenceable id
+    var newRow = $("<section>");
+    newRow.attr("class", `row time-block`);
+    newRow.attr("id", `${rowId[index-9]}`);
+
+    // create a column for the hour
+    var col1 = $("<article>");
+    col1.attr("class", "col-mid-2 hour");
+    col1.text(`${checkHour(index)} ${checkSplit(index)}`);
+
+    // create a column for the notes
+    var text = $("<textarea>");
+    text.attr("class", `col-md-8 ${checkStatus(index)}`);
+    text.text(getSavedInfo(rowId[index-9]));
+
+    // create a column for the save button
+    var save = $("<button>");
+    save.attr("class", "col-md-2 saveBtn");
+    // on click, grab the referenceable id of the row to use as a localStorage key
+    // also grab the value in the previous field (the note field), subtracting the white space
+    save.on("click", function(){ 
+        saveInfo(($(this).parent().attr("id")), ($.trim($(this).prev().val())));
+        resetPage();
+        });
+    save.text("Save");
+
+    // add the columns to the row
+    newRow.append(col1);
+    newRow.append(text);
+    newRow.append(save);
+
+    // add the row to the container
+    $(".container").append(newRow);
+}
+
+// check if the listed time is in the past, present or future
+function checkStatus(hour){
+    if(hour < moment().format("HH")){
+        return ("past");
+    }
+    if(hour > moment().format("HH")){
+        return ("future");
+    }
+    return ("present");
+}
+
+// get if the time is AM or PM
+function checkSplit(hour){
+    if(hour > 11){
+        return ("PM");
+    }
+    return ("AM");
+}
+
+// convert the index to a usable hour
+function checkHour(hour){
+    if(hour > 12){
+        return (hour - 12);
+    }
+    return hour;
+}
+
+// create a button to clear the page contents
+function addClear(){
+    // add a new row
+    var endRow = $("<section>");
+    endRow.attr("class", `row`);
+
+    // create an empty column for spacing purposes
+    var emptyOneCol = $("<article>");
+    emptyOneCol.attr("class", `col-md-2`);
+
+    // create a column to house the clear button
+    var finalCol = $("<div>");
+    finalCol.attr("class", `col-md-6`);
+
+    //create the clear button
+    var clear = $("<button>");
+    clear.text("clear all");
+    clear.attr("class", "btn-block");
+    clear.on("click", function() { clearAll() });
+
+    // add the clear button to the final column
+    finalCol.append(clear);
+
+    // add the empty and final column to the new row
+    endRow.append(emptyOneCol, finalCol);
+
+    // add the new row to the container
+    $(".container").append(endRow);
+}
+
+// clear the local storage and page contents
 function clearAll(){
     localStorage.clear();
     resetPage();
 }
 
+// save the current hour's text to local storage
 function saveInfo(key, text){
+    // if there's no text, don't bother saving
+    if(text === ""){
+        return;
+    }
     var storage = JSON.stringify(text);
     localStorage.setItem(key, storage);
 }
 
+// clear current page containers and refresh
 function resetPage(){
-    holder.empty();
+    $("#currentDay").empty();
+    $(".container").empty();
     init();
 }
 
+// pull any local storage information
 function getSavedInfo(key){
-    var stored = "";
     if(JSON.parse(localStorage.getItem(key))){
         if((JSON.parse(localStorage.getItem(key)).length !== 0)){
-            stored = (JSON.parse(localStorage.getItem(key)));
+            return(JSON.parse(localStorage.getItem(key)));
         }
     }
-    return stored;
+    return "";
 }
 
 resetPage();
